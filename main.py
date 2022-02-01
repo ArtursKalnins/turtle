68dc3c1965f8dd38abf61a7c1120b81584f75acf
Arturs Kalnins <90258773+ArtursKalnins@users.noreply.github.com>
2022-02-01 13:43:52 +0000

    papildinājums

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import - turtle
+import turtle
 Rafael = turtle.Turtle()
 Rafael.shape("turtle")
 Rafael.color("green")
-Rafael.circle(100,steps=6)
-Rafael.forward(100)
-Rafael.color("blue")
-Rafael.circle(50)
-Rafael.right(90)
+Rafael.circle(100, steps=4)
+Rafael.penup()
 Rafael.backward(50)
-Rafael.color("red")
-Rafael.circle(50)
+Rafael.pendown()
+Rafael.circle(100, steps=4)
